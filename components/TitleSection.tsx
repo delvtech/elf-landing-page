d254cb83a9afb0f9347b2726bf82d6c2a1f00fab
Danny Delott <dannydelott@gmail.com>
2021-03-29 15:42:42 -0700

    Remove some headers

--- components/TitleSection.tsx
+++ components/TitleSection.tsx
@@ -17,8 +17,6 @@ async function postData(url = "", data = {}) {
     headers: {
       "Content-Type": "application/json",
     },
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
     body: JSON.stringify(data), // body data type must match "Content-Type" header
   });
   return response.json(); // parses JSON response into native JavaScript objects
