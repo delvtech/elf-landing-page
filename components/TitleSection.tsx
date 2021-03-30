065b0c99382a786c18bf25b8d2d6755fc5ece7f0
Danny Delott <dannydelott@gmail.com>
2021-03-30 00:57:33 -0700

    Fix email intake form

--- components/TitleSection.tsx
+++ components/TitleSection.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useCallback, useState } from "react";
+import React, { ChangeEvent, CSSProperties, useState } from "react";
 import { Box, Button, Grid, Heading, Input, Text } from "theme-ui";
 
 import { Desktop } from "./Desktop";
@@ -7,24 +7,8 @@ import { Mobile } from "./Mobile";
 // eslint-disable-next-line
 const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
-async function postData(url = "", data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: "POST",
-    cache: "no-cache",
-    mode: 'no-cors',
-    credentials: "same-origin",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-
 function Email() {
-  const [email, setEmail] = useState<string>("");
-  const [emailSubmitted, setEmailWasSubmitted] = useState(false);
+  const [email, setEmail] = useState<string | undefined>();
 
   const emailError = !!email && !EMAIL_REGEX.test(email);
 
@@ -32,19 +16,6 @@ function Email() {
     const value = e.target.value;
     setEmail(value);
   }
-
-  const submitEmailForm = useCallback(() => {
-    // Example POST method implementation:
-    postData(
-      `https://emailoctopus.com/api/1.5/lists/bec30112-0e12-11eb-a3d0-06b4694bee2a/contacts?apikey=${process.env.NEXT_PUBLIC_EMAIL_OCTOPUS_API_KEY}`,
-      {
-        api_key: process.env.NEXT_PUBLIC_EMAIL_OCTOPUS_API_KEY,
-        email_address: email,
-      }
-    ).then(() => {
-      setEmailWasSubmitted(true);
-    });
-  }, [email]);
 
   return (
     <>
@@ -59,28 +30,16 @@ function Email() {
           p={1}
           pl={4}
           mt="96px"
-          columns="auto 140px"
+          columns="auto"
           gap="10px"
         >
-          <Input
-            placeholder="Enter your email"
-            type="email"
-            onChange={handleEmailChange}
-            value={email}
-          />
-          <Button
-            disabled={emailError || emailSubmitted}
-            variant="email"
-            sx={{ minWidth: "140px" }}
-            onClick={submitEmailForm}
-          >
-            {emailSubmitted ? "Submitted" : "Sign Up"}
-          </Button>
+          <EmailForm />
         </Grid>
       </Desktop>
       <Mobile>
         <Grid gap={3} mt="80px">
-          <Box
+          <EmailFormMobile />
+          {/* <Box
             p={1}
             pl={4}
             sx={{
@@ -93,17 +52,15 @@ function Email() {
               placeholder="Enter your email"
               type="email"
               onChange={handleEmailChange}
-              value={email}
             />
           </Box>
           <Button
-            disabled={emailError || emailSubmitted}
+            disabled={emailError}
             variant="email"
             sx={{ minWidth: "140px" }}
-            onClick={submitEmailForm}
           >
-            {emailSubmitted ? "Submitted" : "Sign Up"}
-          </Button>
+            Sign Up
+          </Button> */}
         </Grid>
       </Mobile>
     </>
@@ -146,5 +103,143 @@ export function TitleSection(): JSX.Element {
         <Email />
       </Box>
     </Grid>
+  );
+}
+
+function EmailForm() {
+  return (
+    <div
+      className="emailoctopus-form-wrapper emailoctopus-form-default null"
+      style={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between",
+      }}
+    >
+      <p className="emailoctopus-success-message"></p>
+      <p className="emailoctopus-error-message"></p>
+      <form
+        action="https://emailoctopus.com/lists/bec30112-0e12-11eb-a3d0-06b4694bee2a/members/embedded/1.3/add"
+        method="post"
+        data-message-success="Thanks for subscribing!"
+        data-message-missing-email-address="Your email address is required."
+        data-message-invalid-email-address="Your email address looks incorrect, please try again."
+        data-message-bot-submission-error="This doesn't look like a human submission."
+        data-message-consent-required="Please check the checkbox to indicate your consent."
+        data-message-invalid-parameters-error="This form has missing or invalid fields."
+        data-message-unknown-error="Sorry, an unknown error has occurred. Please try again later."
+        className="emailoctopus-form"
+        style={{
+          display: "flex",
+          flex: 1,
+          alignItems: "center",
+        }}
+      >
+        <Input
+          id="field_0"
+          name="field_0"
+          type="email"
+          required
+          placeholder="Enter your email"
+        />
+        <div aria-hidden="true" className="emailoctopus-form-row-hp">
+          <input
+            type="text"
+            name="hpc4b27b6e-eb38-11e9-be00-06b4694bee2a"
+            tabIndex={-1}
+            autoComplete="nope"
+          />
+        </div>
+        <input type="hidden" name="successRedirectUrl" />
+        <Button
+          variant="email"
+          style={{
+            color: "muted",
+          }}
+          sx={{
+            minWidth: "140px",
+          }}
+          type="submit"
+        >
+          Sign Up
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+function EmailFormMobile() {
+  return (
+    <div
+      className="emailoctopus-form-wrapper emailoctopus-form-default null"
+      style={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between",
+      }}
+    >
+      <p className="emailoctopus-success-message"></p>
+      <p className="emailoctopus-error-message"></p>
+      <form
+        action="https://emailoctopus.com/lists/bec30112-0e12-11eb-a3d0-06b4694bee2a/members/embedded/1.3/add"
+        method="post"
+        data-message-success="Thanks for subscribing!"
+        data-message-missing-email-address="Your email address is required."
+        data-message-invalid-email-address="Your email address looks incorrect, please try again."
+        data-message-bot-submission-error="This doesn't look like a human submission."
+        data-message-consent-required="Please check the checkbox to indicate your consent."
+        data-message-invalid-parameters-error="This form has missing or invalid fields."
+        data-message-unknown-error="Sorry, an unknown error has occurred. Please try again later."
+        className="emailoctopus-form"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          flex: 1,
+          alignItems: "center",
+        }}
+      >
+        <Box
+          p={1}
+          pl={4}
+          sx={{
+            borderRadius: "round",
+            boxShadow: "light",
+            bg: "background",
+          }}
+          style={{ width: "100%", marginBottom: 16 }}
+        >
+          <Input
+            id="field_0"
+            name="field_0"
+            type="email"
+            required
+            placeholder="Enter your email"
+          />
+        </Box>
+        <div aria-hidden="true" className="emailoctopus-form-row-hp">
+          <input
+            type="text"
+            name="hpc4b27b6e-eb38-11e9-be00-06b4694bee2a"
+            tabIndex={-1}
+            autoComplete="nope"
+          />
+        </div>
+        <input type="hidden" name="successRedirectUrl" />
+        <Button
+          variant="email"
+          style={{
+            color: "muted",
+            width: "100%",
+          }}
+          sx={{
+            minWidth: "140px",
+          }}
+          type="submit"
+        >
+          Sign Up
+        </Button>
+      </form>
+    </div>
   );
 }
